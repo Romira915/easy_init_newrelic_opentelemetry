d28b9e24d2af56ae95364be1f77d1c9dc7f0b6ac
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-01-30 16:29:00 +0900

    init

---
+++ src/initialize.rs
@@ -0,0 +1,126 @@
+use opentelemetry::propagation::TextMapPropagator;
+use opentelemetry::trace::TraceError;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::logs::Logger;
+use opentelemetry_sdk::metrics::reader::{DefaultAggregationSelector, DefaultTemporalitySelector};
+use opentelemetry_sdk::metrics::MeterProvider;
+use opentelemetry_sdk::propagation::{
+    BaggagePropagator, TextMapCompositePropagator, TraceContextPropagator,
+};
+use opentelemetry_sdk::trace::Tracer;
+use opentelemetry_sdk::{trace, Resource};
+use std::collections::HashMap;
+
+fn propagator_from_string(
+    v: &str,
+) -> Result<Option<Box<dyn TextMapPropagator + Send + Sync>>, TraceError> {
+    match v {
+        "tracecontext" => Ok(Some(Box::new(TraceContextPropagator::new()))),
+        "baggage" => Ok(Some(Box::new(BaggagePropagator::new()))),
+        _ => Ok(None),
+    }
+}
+
+pub(crate) fn init_propagator() {
+    let value_from_env =
+        std::env::var("OTEL_PROPAGATORS").unwrap_or_else(|_| "tracecontext,baggage".to_string());
+    let propagators: Vec<(Box<dyn TextMapPropagator + Send + Sync>, String)> = value_from_env
+        .split(',')
+        .map(|s| {
+            let name = s.trim().to_lowercase();
+            propagator_from_string(&name).map(|o| o.map(|b| (b, name)))
+        })
+        .collect::<Result<Vec<_>, _>>()
+        .expect("Failed to create propagator.")
+        .into_iter()
+        .flatten()
+        .collect();
+    if !propagators.is_empty() {
+        let (propagators_impl, propagators_name): (Vec<_>, Vec<_>) =
+            propagators.into_iter().unzip();
+        tracing::debug!(target: "otel::setup", OTEL_PROPAGATORS = propagators_name.join(","));
+        let composite_propagator = TextMapCompositePropagator::new(propagators_impl);
+        opentelemetry::global::set_text_map_propagator(composite_propagator);
+    }
+}
+
+pub(crate) fn init_logging(
+    new_relic_otlp_endpoint: &str,
+    new_relic_license_key: &str,
+    new_relic_service_name: &str,
+) -> Logger {
+    opentelemetry_otlp::new_pipeline()
+        .logging()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .http()
+                .with_endpoint(new_relic_otlp_endpoint)
+                .with_headers(HashMap::from([(
+                    "api-key".into(),
+                    new_relic_license_key.into(),
+                )])),
+        )
+        .with_log_config(
+            opentelemetry_sdk::logs::config().with_resource(Resource::new(vec![KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                new_relic_service_name.to_string(),
+            )])),
+        )
+        .install_batch(opentelemetry_sdk::runtime::Tokio)
+        .expect("Failed to create logging controller.")
+}
+
+pub(crate) fn build_metrics_provider(
+    new_relic_otlp_endpoint: &str,
+    new_relic_license_key: &str,
+    new_relic_service_name: &str,
+) -> MeterProvider {
+    opentelemetry_otlp::new_pipeline()
+        .metrics(opentelemetry_sdk::runtime::Tokio)
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .http()
+                .with_endpoint(new_relic_otlp_endpoint)
+                .with_headers(HashMap::from([(
+                    "api-key".into(),
+                    new_relic_license_key.into(),
+                )])),
+        )
+        .with_resource(Resource::new(vec![KeyValue::new(
+            opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+            new_relic_service_name.to_string(),
+        )]))
+        .with_period(std::time::Duration::from_secs(3))
+        .with_timeout(std::time::Duration::from_secs(10))
+        .with_aggregation_selector(DefaultAggregationSelector::new())
+        .with_temporality_selector(DefaultTemporalitySelector::new())
+        .build()
+        .expect("Failed to create metrics provider.")
+}
+
+pub(crate) fn init_tracing(
+    new_relic_otlp_endpoint: &str,
+    new_relic_license_key: &str,
+    new_relic_service_name: &str,
+) -> Tracer {
+    opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .http()
+                .with_endpoint(new_relic_otlp_endpoint)
+                .with_headers(HashMap::from([(
+                    "api-key".into(),
+                    new_relic_license_key.into(),
+                )])),
+        )
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                new_relic_service_name.to_string(),
+            )])),
+        )
+        .install_batch(opentelemetry_sdk::runtime::Tokio)
+        .expect("Failed to create tracer.")
+}
---
+++ src/lib.rs
@@ -0,0 +1,83 @@
+use crate::initialize::{build_metrics_provider, init_logging, init_propagator, init_tracing};
+use time::macros::offset;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+mod initialize;
+
+pub struct NewRelicSubscriberInitializer {
+    new_relic_otlp_endpoint: Option<String>,
+    new_relic_license_key: Option<String>,
+    new_relic_service_name: Option<String>,
+}
+
+impl NewRelicSubscriberInitializer {
+    pub fn new_relic_otlp_endpoint(mut self, new_relic_otlp_endpoint: &str) -> Self {
+        self.new_relic_otlp_endpoint = Some(new_relic_otlp_endpoint.to_string());
+        self
+    }
+
+    pub fn new_relic_license_key(mut self, new_relic_license_key: &str) -> Self {
+        self.new_relic_license_key = Some(new_relic_license_key.to_string());
+        self
+    }
+
+    pub fn new_relic_service_name(mut self, new_relic_service_name: &str) -> Self {
+        self.new_relic_service_name = Some(new_relic_service_name.to_string());
+        self
+    }
+
+    pub fn init(self) {
+        let new_relic_otlp_endpoint = self.new_relic_otlp_endpoint.unwrap_or_default();
+        let new_relic_license_key = self.new_relic_license_key.unwrap_or_default();
+        let new_relic_service_name = self.new_relic_service_name.unwrap_or_default();
+        init_propagator();
+
+        let tracer = init_tracing(
+            &new_relic_otlp_endpoint,
+            &new_relic_license_key,
+            &new_relic_service_name,
+        );
+
+        let fmt_layer = tracing_subscriber::fmt::layer()
+            .with_ansi(true)
+            .with_file(true)
+            .with_line_number(true)
+            .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
+                offset!(+09:00:00),
+                time::format_description::well_known::Rfc3339,
+            ))
+            .with_target(true);
+        let env_filter =
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
+        let otel_trace_layer = tracing_opentelemetry::layer()
+            .with_error_records_to_exceptions(true)
+            .with_error_fields_to_exceptions(true)
+            .with_error_events_to_status(true)
+            .with_error_events_to_exceptions(true)
+            .with_location(true)
+            .with_tracer(tracer);
+        let otel_metrics_layer = tracing_opentelemetry::MetricsLayer::new(build_metrics_provider(
+            &new_relic_otlp_endpoint,
+            &new_relic_license_key,
+            &new_relic_service_name,
+        ));
+        init_logging(
+            &new_relic_otlp_endpoint,
+            &new_relic_license_key,
+            &new_relic_service_name,
+        );
+        let otel_logs_layer =
+            opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge::new(
+                &opentelemetry::global::logger_provider(),
+            );
+
+        tracing_subscriber::registry()
+            .with(fmt_layer)
+            .with(env_filter)
+            .with(otel_trace_layer)
+            .with(otel_metrics_layer)
+            .with(otel_logs_layer)
+            .init();
+    }
+}
